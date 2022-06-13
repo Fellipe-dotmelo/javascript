8ddeaad6f2ba029f5aca6a0e64cf628910afcd83
Fellipe-dotmelo <61851972+Fellipe-dotmelo@users.noreply.github.com>
2022-06-13 16:18:32 -0300

    tabuada criada
    .

---
+++ tabuada.js
@@ -0,0 +1,29 @@
+function calculadora(){
+    numero = Number(prompt("Digite um número o qual deseja ver a tabuada: "))
+}
+
+
+programa
+{
+	
+	funcao inicio()
+	{
+		inteiro numero, contador, limite, resultado
+	
+		escreva()
+		leia(numero)
+
+		escreva("Qual o limite da tabuada? ")
+		leia(limite)
+
+		escreva("\n")
+		
+		contador = 0
+
+		faca{
+			resultado = numero * contador
+			escreva(numero + " x " + contador + " = " + resultado + "\n")
+			contador++
+		}enquanto(contador <= limite)
+	}
+}
